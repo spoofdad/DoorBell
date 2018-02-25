507d94752a4b7918bd72ae0b511739d364faa324
spoofdad <richburcham@gmail.com>
2018-02-24 21:11:39 -0600

    The Doorbell Example

---
+++ DoorBell.java
@@ -0,0 +1,29 @@
+import java.io.*;
+public class DoorBell {
+
+String visiter;
+String owner;
+
+// This is the constructor of the class DoorBell
+
+	public DoorBell(String visiterName, String ownerName) {
+		this.visiter=visiterName;
+		this.owner=ownerName;
+	}
+
+	public void whoIsIt() {	
+		System.out.println("Hi, who is it ");
+
+	} 
+
+	public void answerOwner() {	
+		System.out.println("Hi, this is " + this.visiter);
+
+	} 
+
+	public void answerVisiter() {
+		System.out.println("Hi, this is " + this.owner + " common on it!");
+	}
+	
+
+}
---
+++ DoorBellRings.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class DoorBellRings {
+
+
+
+	public static void main(String args [] ) {
+		System.out.println("DoorBell Rings");
+		String owner = "PapaDogs";
+		String visiter = "Mamafluffs"; 
+
+		/* Create Doorbell objects using constructors */ 
+		DoorBell ringDoor = new DoorBell (visiter, owner);
+	
+		/* Invoke methods for Doorbell */
+		ringDoor.whoIsIt();
+		ringDoor.answerOwner();
+		ringDoor.answerVisiter();
+
+	}
+}
+
+
